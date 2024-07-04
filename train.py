5b930568adc2c5ed4c07b3545a0adb836fd5d94d
Bea Maia <beamaia.work@gmail.com>
2024-07-04 16:47:58 -0300

    change name to main

--- train.py
+++
@@ -1,6 +0,0 @@
-import pathlib as pl
-import os
-
-PATCH_PATH = pl.Path(os.path.abspath("")) / pl.Path('../data') / pl.Path('patches')
-IMAGE_PATH = pl.Path(os.path.abspath("")) / pl.Path('../data') / pl.Path('images') 
-METADATA_PATH = pl.Path(os.path.abspath("")) / pl.Path('../data') / pl.Path('ndb-ufes.csv') 
