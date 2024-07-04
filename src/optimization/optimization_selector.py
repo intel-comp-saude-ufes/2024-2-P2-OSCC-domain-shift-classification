dd789385940903c3599bfb6bdb00ac2cc99c73bb
Bea Maia <beamaia.work@gmail.com>
2024-07-04 19:15:31 -0300

    fix optimizer selectio

--- src/optimization/optimization_selector.py
+++ src/optimization/optimization_selector.py
@@ -2,11 +2,10 @@ from torch.optim import Adam, SGD
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 
 class OptimizationSelector:
-    def __init__(self, optimizer_name, scheduler_name, learning_rate, weight_decay, **kwargs):
-        self.optimization_name = optimizer_name
-        self.scheduler_name = scheduler_name
+    def __init__(self, optimizer_name, scheduler_name, learning_rate, **kwargs):
+        self.optimization_name = optimizer_name.lower()
+        self.scheduler_name = scheduler_name.lower()
         self.learning_rate = learning_rate
-        self.weight_decay = weight_decay
         self.kwargs = kwargs
 
         self.optimizer = None
@@ -14,27 +13,29 @@ class OptimizationSelector:
 
     def get_optimizer(self, model_parameters):
         if self.optimization_name == 'adam':
-            self.optimizer = Adam(model_parameters, lr=self.learning_rate, weight_decay=self.weight_decay)
+            self.optimizer = Adam(model_parameters, lr=self.learning_rate)
         elif self.optimization_name == 'sgd':
-            self.optimizer = SGD(model_parameters, lr=self.learning_rate, weight_decay=self.weight_decay)
+            self.optimizer = SGD(model_parameters, lr=self.learning_rate)
         else:
             raise ValueError('Invalid optimization name')
         
         return self.optimizer
 
-    def get_scheduler(self, optimizer):
+    def get_scheduler(self):
         if not self.optimizer:
             raise ValueError('Optimizer is not initialized')
         
-        if self.step_size and self.gamma:
-            step_size = self.kwargs.step_size.get('step_size', 30)
-            gamma = self.kwargs.gamma.get('gamma', 0.1)
-            self.scheduler = StepLR(optimizer, step_size=step_size, gamma=gamma)
+        if self.scheduler_name == 'step_lr':
+            step_size = self.kwargs.get('step_size', 30)
+            gamma = self.kwargs.get('gamma', 0.1)
+            self.scheduler = StepLR(self.optimizer, step_size=step_size, gamma=gamma)
+        elif self.scheduler_name == 'reduce_lr_on_plateau':
+            mode = self.kwargs.get('mode', 'min')
+            factor = self.kwargs.get('factor', 0.1)
+            patience = self.kwargs.get('patience', 10)
+            min_lr = self.kwargs.get('min_lr', 10e-6)
+            self.scheduler = ReduceLROnPlateau(self.optimizer, mode=mode, factor=factor, patience=patience, min_lr=min_lr)
         else:
-            mode = self.kwargs.mode.get('mode', 'min')
-            factor = self.kwargs.factor.get('factor', 0.1)
-            patience = self.kwargs.patience.get('patience', 10)
-            verbose = self.kwargs.verbose.get('verbose', True)
-            self.scheduler = ReduceLROnPlateau(optimizer, mode=mode, factor=factor, patience=patience, verbose=verbose)
+            raise ValueError('Invalid scheduler name')
 
         return self.scheduler
