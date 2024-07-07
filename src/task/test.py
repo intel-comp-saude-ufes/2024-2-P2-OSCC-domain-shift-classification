fix weight computation according to dataset classes

src/task/test.py
@@ -84,7 +84,7 @@ class TestTask:
         test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
 
         if self.use_loss_weights:
-                class_weights = compute_class_weight('balanced', classes=np.array([0, 1]), y=test.labels)
+                class_weights = compute_class_weight('balanced', classes=np.array(list(self.dataset.labels_names.keys())), y=test.labels)
                 class_weights = torch.tensor(class_weights, dtype=torch.float).to(self.device)
                 loss_selector = LossSelector(self.loss_name, class_weights)
         else:

src/task/train.py
@@ -110,7 +110,7 @@ class TrainTask:
             scheduler = optimizer_selector.get_scheduler()
 
             if self.use_loss_weights:
-                class_weights = compute_class_weight('balanced', classes=np.array([0, 1]), y=train_dataset.labels)
+                class_weights = compute_class_weight('balanced', classes=np.array(list(self.dataset.labels_names.keys())), y=train_dataset.labels)
                 class_weights = torch.tensor(class_weights, dtype=torch.float).to(self.device)
                 loss_selector = LossSelector(self.loss_name, class_weights)
             else: